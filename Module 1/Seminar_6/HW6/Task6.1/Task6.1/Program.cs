013051dd855f11f02c3a6cb61413000bbc2507d5
ParanormalParrot <87578834+ParanormalParrot@users.noreply.github.com>
2021-10-04 16:27:04 +0300

    Added HW6

---
+++ Module 1/Seminar_6/HW6/Task6.1/Task6.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task6._1
+{
+    class Program
+    {
+        public static void Compress(ref int[] arr)
+        {
+            int len = arr.Length;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (Math.Abs(arr[i] + arr[i + 1]) % 3 == 0)
+                {
+                    arr[i] = arr[i] * arr[i + 1];
+                    for (int j = i + 1; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+
+                    }
+                    Array.Resize(ref arr, arr.Length - 1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (int.TryParse(s, out int n))
+            {
+                Random rand = new Random();
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    numbers[i] = rand.Next(-10, 10);
+
+                }
+                Array.ForEach(numbers, i => Console.Write(i + " "));
+                Console.WriteLine();
+                Compress(ref numbers);
+                Array.ForEach(numbers, i => Console.Write(i + " "));
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Module 1/Seminar_6/HW6/Task6.2/Task6.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Task6._2
+{
+
+    class Program
+    {
+        public static void DeleteEvenNumbers(ref int[] arr)
+        {
+            int newSize = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]%2 != 0) newSize++;
+            }
+            Array.Sort(arr, (a, b) =>
+            {
+                if (Math.Abs(a) % 2 == 0 && Math.Abs(b) % 2 == 1) return 1;
+                if (Math.Abs(a) % 2 == 1 && Math.Abs(b) % 2 == 0) return -1;
+                else return 0;
+
+
+            });
+            Array.Resize(ref arr, newSize);
+
+
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (int.TryParse(s, out int n))
+            {
+                Random rand = new Random();
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    numbers[i] = rand.Next(-10, 10);
+
+                }
+                Array.ForEach(numbers, i => Console.Write(i + " "));
+                Console.WriteLine();
+                DeleteEvenNumbers(ref numbers);
+                Array.ForEach(numbers, i => Console.Write(i + " "));
+
+            }
+        }
+    }
+}
