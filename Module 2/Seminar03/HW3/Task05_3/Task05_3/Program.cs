916d9d5a2c63103474e5f18707375ff7e215f16c
ParanormalParrot <87578834+ParanormalParrot@users.noreply.github.com>
2021-11-07 19:43:12 +0300

    Added HW3.

---
+++ Module 2/Seminar03/HW3/Task05_3/Task05_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Task05_3
+{
+    class Program
+    {
+        public static string GenerateName()
+        {
+            string name = "";
+            var rand = new Random();
+            int len = rand.Next(2, 10);
+            int t;
+            t = rand.Next(0, 2);
+            for (int i = 0; i < len; i++)
+            {
+                t = rand.Next(0, 2);
+                if (t == 0)
+                {
+                    name += Convert.ToChar(rand.Next(65, 91));
+                }
+
+                if (t == 1)
+                {
+                    name += Convert.ToChar(rand.Next(97, 123));
+                }
+            }
+
+            return name;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var rand = new Random();
+                var vf = new VideoFile(GenerateName(), rand.Next(60, 361),
+                    rand.Next(100, 1001));
+                Console.WriteLine(vf);
+                int n = rand.Next(5, 15);
+                var array = new VideoFile[n];
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = new VideoFile(GenerateName(), rand.Next(60, 361),
+                        rand.Next(100, 1001));
+                    if (array[i].Size > vf.Size)
+                    {
+                        Console.WriteLine(array[i]);
+                    }
+                }
+                Console.WriteLine("Нажмите ESCAPE, чтобы выйти. Чтобы заново запустить прорамму, нажмите любую другую кнопку.");
+                if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Module 2/Seminar03/HW3/Task05_3/Task05_3/VideoFile.cs
@@ -0,0 +1,24 @@
+﻿namespace Task05_3
+{
+    public class VideoFile
+    {
+        public string _name;
+        public int _duration;
+        public int _quality;
+
+        public VideoFile(string name, int duration, int quality)
+        {
+            _name = name;
+            _duration = duration;
+            _quality = quality;
+        }
+
+        public int Size => _duration * _quality;
+
+        public override string ToString()
+        {
+            return (
+                $"Наименование видеофайла: {_name}\nДлительность в секундах: {_duration}\nКачество видеофайла: {_quality}\nРазмер видеофайла: {Size}\n");
+        }
+    }
+}
